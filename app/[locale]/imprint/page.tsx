d496ee369254c55774e0dce0bca4bcfda7a3469b
FirstLearnJava <mat.floimayr@derflorianer.at>
2025-04-22 17:01:07 +0200

    Add impress background & got rid of darkmode

--- app/[locale]/imprint/page.tsx
+++ app/[locale]/imprint/page.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from 'next-intl';
 export default function ImprintPage() {
   const t = useTranslations('Impress');
   return (
-    <div className="flex justify-center font-mont text-base font-medium mt-[62px] pb-8 sm:pb-5">
+    <div className="flex justify-center font-mont text-base font-medium mt-[62px] pb-8 sm:pb-5 bg-projectBackground">
       <div className="flex flex-col gap-3 sm:gap-2 w-[80%] xxl:w-[86%] lg:w-full lg:px-9 md:px-7">
         <h1 className="flex justify-center mb-8 sm:mb-3 text-[42px] lg:text-[40px] md:text-[38px] sm:text-[34px] font-mont font-semibold mt-[4.5rem] lg:mt-16 sm:mt-12">
           {t('impressTitle')}
